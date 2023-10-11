4a603e3b696b2bf8a3d4549f8b13f288aa842007
Tanish kapoor <kapoortanish2022@gmail.com>
2023-10-11 19:53:03 +0530

    update

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -4,8 +4,8 @@ using namespace std;
 void merge(int array[], int lb, int mid, int ub) {
     int i = lb;
     int j = mid + 1;
-    int k = 0; // Index for the temporary array
-    int temp[ub - lb + 1]; // Size of temp array is (ub - lb + 1)
+    int k = 0; 
+    int temp[ub - lb + 1];
 
     while (i <= mid && j <= ub) {
         if (array[i] <= array[j]) {
@@ -29,14 +29,10 @@ void merge(int array[], int lb, int mid, int ub) {
         j++;
         k++;
     }
-
-    // Copy elements from temp back to the original array
     for (k = 0; k <= (ub - lb); k++) {
         array[lb + k] = temp[k];
     }
 }
-
-
 void mergeSort(int array[], int lb, int ub) {
     if (lb < ub) {
         int mid = (lb + ub) / 2;
@@ -45,7 +41,6 @@ void mergeSort(int array[], int lb, int ub) {
         merge(array, lb, mid, ub);
     }
 }
-
 int main() {
     int array[] = {12, 11, 13, 5, 6, 7};
     int size = sizeof(array) / sizeof(array[0]);
@@ -55,14 +50,11 @@ int main() {
         cout << array[i] << " ";
     }
     cout << endl;
-
     mergeSort(array, 0, size - 1);
-
     cout << "Sorted array: ";
     for (int i = 0; i < size; i++) {
         cout << array[i] << " ";
     }
     cout << endl;
-
     return 0;
 }
